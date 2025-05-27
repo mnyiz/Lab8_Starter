03d97881d99091132bf72b8819ff8a56169bcc5a
minnie <minniezhang0510@gmail.com>
2025-05-26 18:12:43 -0700

    part2 B1-B5

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -54,6 +54,16 @@ function initializeServiceWorker() {
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if('serviceWorker' in navigator){
+    window.addEventListener('load', ()=>{
+      navigator.serviceWorker.register('./sw.js').then((registration) =>{
+        console.log("Service worker registered", registration.scope);
+      })
+      .catch((error) =>{
+        console.log("Service worker registration failed", error);
+      });
+    });
+  }
 }
 
 /**
